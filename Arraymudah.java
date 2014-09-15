e51aff5c92ed8651156c38f708cc68fb41b3ca2d
Negara74 <bakekok053@gmail.com>
2014-09-15 17:00:33 +0700

    Muhammad Robani Nur RPLL

---
+++ Arraymudah.java
@@ -0,0 +1,12 @@
+public class arraymudah
+{
+		public static void main(String[]args)
+		{
+				int[]ages=new int[10];
+				for(int i=0;i<=10;i++)
+				{
+						System.out.println(i+"tambah"+i+"="+(i+i));
+				}
+		}
+		
+}
